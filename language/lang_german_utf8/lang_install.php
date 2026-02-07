55ebeb4a7db2dd9a894ab0da69bc972f0e3a55a0
Jürgen Grüneisl <mopzz@m-gb.org>
2026-02-07 19:43:54 +0100

    reworked german language files

--- language/lang_german_utf8/lang_install.php
+++ language/lang_german_utf8/lang_install.php
@@ -1,7 +1,7 @@
 <?php
 	/*
 	MGB 0.7.x - OpenSource PHP and MySql Guestbook
-	Copyright (C) 2004 - 2013 Juergen Grueneisl - http://www.m-gb.org/
+	Copyright (C) 2004 - 2026 Juergen Grueneisl - https://www.m-gb.org/
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -22,83 +22,82 @@
 	=========================
 	*/
 
-	// initiate array $lang
-	$lang = array();
-
 	// header
-	$lang['h_title'] = "Willkommen zur Installation des MGB {MGB_VERSION}";
+	$lang = [
+		'h_title' 	=> "Willkommen zur Installation des MGB {MGB_VERSION}",
 
 	// general
-	$lang['next_step'] = "&raquo; Weiter &raquo;";
-	$lang['cancel'] = "Abbruch";
-	$lang['yes'] = "Ja";
-	$lang['no'] = "Nein";
-	$lang['active'] = "Aktiv";
-	$lang['inactive'] = "Inaktiv";
+		'next_step' => "&raquo; Weiter &raquo;",
+		'cancel' 	=> "Abbruch",
+		'yes'		=> "Ja",
+		'no' 		=> "Nein",
+		'active'	=> "Aktiv",
+		'inactive'	=> "Inaktiv",
 
 	// installation
-	$lang['title'] = "Installation des MGB {MGB_VERSION}";
+		'title' 			=> "Installation des MGB {MGB_VERSION}",
 
-	$lang['eula_expl'] = "Das MGB OpenSource Gästebuch wird unter der GNU/GPL Lizenz für freie Software (GPLv2) vertrieben. Wenn Du das Gästebuch nutzen willst, egal zu welchem Zweck, musst Du dich damit einverstanden erklären. Lese die Lizenz bitte sorgfältig durch.";
-	$lang['eula_agree'] = "Ich bin mit den Bedingungen der GNU/GPL einverstanden.";
-	$lang['eula_disagree'] = "Ich bin mit den Bedingungen der GNU/GPL <b>nicht</b> einverstanden.";
+		'eula_expl' 		=> "Das MGB OpenSource Gästebuch wird unter der GNU/GPL Lizenz für freie Software (GPLv2) vertrieben. Wenn Du das Gästebuch nutzen willst, egal zu welchem Zweck, musst Du dich damit einverstanden erklären. Lese die Lizenz bitte sorgfältig durch.",
+		'eula_agree' 		=> "Ich bin mit den Bedingungen der GNU/GPL einverstanden.",
+		'eula_disagree' 	=> "Ich bin mit den Bedingungen der GNU/GPL <b>nicht</b> einverstanden.",
 
-	$lang['thanks'] = "Danke dass Du der GNU/GPL zugestimmt, und dich für das MGB OpenSource Gästebuch entschieden hast.";
-	$lang['expl_step1'] = "Hier werden einige wichtige Informationen angezeigt, die für die Installation notwendig sind. Du kannst anhand der Symbole sofort sehen, ob sich das MGB auf Deinem Server installieren läßt.";
-	$lang['expl_step2'] = "Bitte gib nun die Zugangsdaten für Deine Datenbank, sowie für das anzulegende Administratorkonto an.";
-	$lang['expl_step3'] = "Herzlichen Glückwunsch! Das MGB ".MGB_VERSION." wurde erfolgreich auf Deinem Server installiert. Bitte lösche jetzt zu Deiner eigenen Sicherheit das Verzeichnis ''install''. Alternativ dazu kannst Du es auch umbenennen.<br><br>Du kannst dich nun in die Administration einloggen. Es wird empfohlen Deinen Benutzernamen umzubenennen, falls Du den Standardnamen ''admin'' benutzt hast.<br><br>Viel Spaß mit Deinem neuen Gästebuch! :)";
-	$lang['expl_step3_fail'] = "Ein Fehler ist aufgetreten. Bitte starte die Installation von vorne, und überprüfe Deine angegebenen Daten noch einmal.<br><br>Sollte die Installation erneut fehlschlagen, findest Du hier Hilfe:<br><a href='http://forum.m-gb.org/' target='_blank'>Forum des MGB OpenSource Gästebuches</a>";
+		'thanks' 			=> "Danke dass Du der GNU/GPL zugestimmt, und dich für das MGB OpenSource Gästebuch entschieden hast.",
+		'expl_step1' 		=> "Hier werden einige wichtige Informationen angezeigt, die für die Installation notwendig sind. Du kannst anhand der Symbole sofort sehen, ob sich das MGB auf Deinem Server installieren läßt.",
+		'expl_step2' 		=> "Bitte gib nun die Zugangsdaten für Deine Datenbank, sowie für das anzulegende Administratorkonto an.",
+		'expl_step3' 		=> "Herzlichen Glückwunsch! Das MGB ".MGB_VERSION." wurde erfolgreich auf Deinem Server installiert. Bitte lösche jetzt zu Deiner eigenen Sicherheit das Verzeichnis ''install''. Alternativ dazu kannst Du es auch umbenennen.<br><br>Du kannst dich nun in die Administration einloggen. Es wird empfohlen Deinen Benutzernamen umzubenennen, falls Du den Standardnamen ''admin'' benutzt hast.<br><br>Viel Spaß mit Deinem neuen Gästebuch! :)",
+		'expl_step3_fail' 	=> "Ein Fehler ist aufgetreten. Bitte starte die Installation von vorne, und überprüfe Deine angegebenen Daten noch einmal.<br><br>Sollte die Installation erneut fehlschlagen, findest Du hier Hilfe:<br><a href='http://forum.m-gb.org/' target='_blank'>Forum des MGB OpenSource Gästebuches</a>",
 
 	// step 1
-	$lang['srvcfg_server'] = "Server:";
-	$lang['srvcfg_phpversion'] = "PHP Version:";
-	$lang['srvcfg_mysqlversion'] = "MySQL Version:";
-	$lang['srvcfg_mysqliversion'] = "MySQLi-Erweiterung:";
-	$lang['srvcfg_gd'] = "GD Bibliothek:";
-	$lang['srvcfg_writable'] = "Schreibrechte:";
-	$lang['srvcfg_reg_globals'] = "register_globals:";
+		'srvcfg_server' 		=> "Server:",
+		'srvcfg_phpversion' 	=> "PHP Version:",
+		'srvcfg_mysqlversion' 	=> "MySQL Version:",
+		'srvcfg_mysqliversion' 	=> "MySQLi-Erweiterung:",
+		'srvcfg_gd' 			=> "GD Bibliothek:",
+		'srvcfg_writable' 		=> "Schreibrechte:",
+		'srvcfg_reg_globals' 	=> "register_globals:",
 
 	// errormessages step 1
-	$lang['error_1'] = "Deine PHP Version ist älter als die erforderliche. Eine Aktualisierung wird empfohlen.<br><br><b>Setze die Variable ''".chr(36)."ignore_warnings'' in Zeile 61 der install.php auf '1', um trotz der Warnung fortzufahren.</b>";
-	$lang['error_2'] = "Deine MySQL Version ist älter als die erforderliche. Eine Aktualisierung wird empfohlen.<br><br><b>Setze die Variable ''".chr(36)."ignore_warnings'' in Zeile 61 der install.php auf '1', um trotz der Warnung fortzufahren.</b>";
-	$lang['error_3'] = "Die GD Bibliothek ist nicht verfügbar. Das Gästebuch kann betrieben werden, jedoch ohne Sicherheitsabfrage.";
-	$lang['error_4'] = "Stelle bitte sicher, dass die Ordner 'includes', 'cache' und 'save' schreibbar sind.";
-	$lang['error_5'] = "register_globals ist aktiviert. Dies birgt ein Sicherheitsrisiko. Es sollte deaktiviert werden.";
-	$lang['error_6'] = "Die PHP Erweiterung mysqli ist nicht vorhanden oder deaktiviert.";
-	$lang['error_7'] = "Deine MySQL Version kann leider nicht ermittelt werden. Läuft dein MySQL-Server?";
-	$lang['no_error'] = "Alle Werte sind OK! Klicke bitte auf ''Weiter''";
+		'error_1' 	=> "Deine PHP Version ist älter als die erforderliche. Eine Aktualisierung wird empfohlen.<br><br><b>Setze die Variable ''".chr(36)."ignore_warnings'' in Zeile 61 der install.php auf '1', um trotz der Warnung fortzufahren.</b>",
+		'error_2' 	=> "Deine MySQL Version ist älter als die erforderliche. Eine Aktualisierung wird empfohlen.<br><br><b>Setze die Variable ''".chr(36)."ignore_warnings'' in Zeile 61 der install.php auf '1', um trotz der Warnung fortzufahren.</b>",
+		'error_3' 	=> "Die GD Bibliothek ist nicht verfügbar. Das Gästebuch kann betrieben werden, jedoch ohne Sicherheitsabfrage.",
+		'error_4'	=> "Stelle bitte sicher, dass die Ordner 'includes', 'cache' und 'save' schreibbar sind.",
+		'error_5' 	=> "register_globals ist aktiviert. Dies birgt ein Sicherheitsrisiko. Es sollte deaktiviert werden.",
+		'error_6' 	=> "Die PHP Erweiterung mysqli ist nicht vorhanden oder deaktiviert.",
+		'error_7' 	=> "Deine MySQL Version kann leider nicht ermittelt werden. Läuft dein MySQL-Server?",
+		'no_error' 	=> "Alle Werte sind OK! Klicke bitte auf ''Weiter''",
 
 	// step 2
-	$lang['db_title'] = "Datenbankinformationen:";
-	$lang['db_hostname'] = "Host:";
-	$lang['db_dbname'] = "Datenbankname:";
-	$lang['db_username'] = "Benutzername:";
-	$lang['db_password'] = "Passwort:";
-	$lang['db_prefix'] = "Tabellenpräfix:";
+		'db_title' 		=> "Datenbankinformationen:",
+		'db_hostname' 	=> "Host:",
+		'db_dbname' 	=> "Datenbankname:",
+		'db_username' 	=> "Benutzername:",
+		'db_password' 	=> "Passwort:",
+		'db_prefix' 	=> "Tabellenpräfix:",
 
-	$lang['admin_title'] = "Administratorkonto:";
-	$lang['admin_name'] = "Name:";
-	$lang['admin_username'] = "Benutzername:";
-	$lang['admin_password'] = "Passwort:";
-	$lang['admin_password_repeat'] = "Passwort wiederholen:";
-	$lang['admin_email'] = "eMail:";
-	$lang['admin_gbemail'] = "Gästebuch eMail:";
-	
-	$lang['allow_telemetry'] = "Anonyme Nutzungsdaten<br>erlauben.<br><br>Erfasst wird:<br><br>- MGB Version<br>- PHP Version<br>- SQL Version<br>- Anonyme Install ID<br>- Aktivitätsping";
+		'admin_title' 			=> "Administratorkonto:",
+		'admin_name' 			=> "Name:",
+		'admin_username' 		=> "Benutzername:",
+		'admin_password' 		=> "Passwort:",
+		'admin_password_repeat' => "Passwort wiederholen:",
+		'admin_email' 			=> "eMail:",
+		'admin_gbemail' 		=> "Gästebuch eMail:",
+		
+		'allow_telemetry' 		=> "Anonyme Nutzungsdaten<br>erlauben.<br><br>Erfasst wird:<br><br>- MGB Version<br>- PHP Version<br>- SQL Version<br>- Anonyme Install ID<br>- Aktivitätsping",
 
-	$lang['post_admin_name'] = "Webmaster";
-	$lang['post_admin_username'] = "admin";
+		'post_admin_name' 		=> "Webmaster",
+		'post_admin_username' 	=> "admin",
 
 	// errormessages step 2
-	$lang['error_1_step2'] = "Bitte fülle alle Felder aus!";
-	$lang['error_2_step2'] = "Mindestens eine der beiden eMail Adressen ist ungültig.";
-	$lang['error_3_step2'] = "Die Verbindung zur Datenbank ist fehlgeschlagen, überprüfe Deine Zugangsdaten.";
-	$lang['error_4_step2'] = "Es befindet sich bereits eine Installation mit dem angegebenen Präfix in der Datenbank. Bitte wähle einen anderen Präfix.";
-	$lang['error_5_step2'] = "Der angegebene Präfix enthält Sonderzeichen und ist somit ungültig. Erlaubt ist nur ein Unterstrich (_).";
-	$lang['error_6_step2'] = "Der angegebene Benutzername enthält Sonderzeichen und ist somit ungültig. Erlaubt sind lediglich Klein- und Großbuchstaben, sowie Zahlen.";
-	$lang['error_7_step2'] = "Die beiden Admin-Passwörter stimmen nicht überein.";
+		'error_1_step2' => "Bitte fülle alle Felder aus!",
+		'error_2_step2' => "Mindestens eine der beiden eMail Adressen ist ungültig.",
+		'error_3_step2' => "Die Verbindung zur Datenbank ist fehlgeschlagen, überprüfe Deine Zugangsdaten.",
+		'error_4_step2' => "Es befindet sich bereits eine Installation mit dem angegebenen Präfix in der Datenbank. Bitte wähle einen anderen Präfix.",
+		'error_5_step2' => "Der angegebene Präfix enthält Sonderzeichen und ist somit ungültig. Erlaubt ist nur ein Unterstrich (_).",
+		'error_6_step2' => "Der angegebene Benutzername enthält Sonderzeichen und ist somit ungültig. Erlaubt sind lediglich Klein- und Großbuchstaben, sowie Zahlen.",
+		'error_7_step2' => "Die beiden Admin-Passwörter stimmen nicht überein.",
 
-	$lang['to_administration'] = "Zur Administration";
-	$lang['to_guestbook'] = "Zum Gästebuch";
-	$lang['to_install'] = "Erneute Installation versuchen";
+		'to_administration' => "Zur Administration",
+		'to_guestbook' 		=> "Zum Gästebuch",
+		'to_install' 		=> "Erneute Installation versuchen"
+	];
 ?>
--- language/lang_german_utf8/lang_main.php
+++ language/lang_german_utf8/lang_main.php
@@ -1,7 +1,7 @@
 <?php
 	/*
 	MGB 0.7.x - OpenSource PHP and MySql Guestbook
-	Copyright (C) 2004 - 2013 Juergen Grueneisl - http://www.m-gb.org/
+	Copyright (C) 2004 - 2026 Juergen Grueneisl - https://www.m-gb.org/
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -22,128 +22,130 @@
 	======================
 	*/
 
-	// initiate array $lang
-	$lang = array();
-
+	$lang = [
+	
 	// INDEX.PHP
-	$lang['install_directory_exists'] = "Das Installationsverzeichnis wurde noch nicht gelöscht.<br>Zu Deiner eigenen Sicherheit solltest Du das jetzt tun!<br>Vergiss nach einem Update aber nicht die <a href=\"install/upgrade.php\" title=\"Upgrade\">upgrade.php</a> auszuführen!";
-	$lang['new_entry'] = "Eintragen";
-	$lang['new_entry_descr'] = "Hier kannst Du einen neuen Gästebucheintrag verfassen";
-	$lang['contact'] = "Kontakt";
-	$lang['contact_descr'] = "Hier kannst Du Kontakt mit dem Administrator aufnehmen";
-	$lang['adminpanel'] = "Administration";
-	$lang['adminpanel_descr'] = "Zum Login";
-	$lang['entry'] = "Eintrag";
-	$lang['entries'] = "Einträge";
-	$lang['no_entries'] = "Es wurden leider noch keine<br>Einträge hinterlassen.";
-	$lang['entries_on_pages'] = "Einträge auf {PAGES} Seiten";
-	$lang['page_first'] = "Zur ersten Seite";
-	$lang['page_first_symbol'] = "&laquo;";
-	$lang['page_forwards'] = "Eine Seite vorwärts blättern";
-	$lang['page_forwards_symbol'] = "&rsaquo;";
-	$lang['page_last'] = "Zur letzten Seite";
-	$lang['page_last_symbol'] = "&raquo;";
-	$lang['page_backwards'] = "Eine Seite zurück blättern";
-	$lang['page_backwards_symbol'] = "&lsaquo;";
-	$lang['anchor']	= "Direkt zu diesem Eintrag springen";
-	$lang['from'] = "aus";
-	$lang['at'] = "um";
-	$lang['oclock'] = "Uhr";
-	$lang['comment'] = "Kommentar";
-	$lang['email_yes'] = "eMail von {ENTRY_NAME}";
-	$lang['email_no'] = "{ENTRY_NAME} möchte keine eMails über das Gästebuch empfangen.";
-	$lang['hp_of'] = "Homepage von {ENTRY_NAME}";
-	$lang['gravatar'] = "Gravatar von {ENTRY_NAME}";
-	$lang['quote'] = "Zitat von";
+		'install_directory_exists' 	=> "Das Installationsverzeichnis wurde noch nicht gelöscht.<br>Zu Deiner eigenen Sicherheit solltest Du das jetzt tun!<br>Vergiss nach einem Update aber nicht die <a href=\"install/upgrade.php\" title=\"Upgrade\">upgrade.php</a> auszuführen!",
+		'new_entry' 				=> "Eintragen",
+		'new_entry_descr' 			=> "Hier kannst Du einen neuen Gästebucheintrag verfassen",
+		'contact' 					=> "Kontakt",
+		'contact_descr' 			=> "Hier kannst Du Kontakt mit dem Administrator aufnehmen",
+		'adminpanel' 				=> "Administration",
+		'adminpanel_descr' 			=> "Zum Login",
+		'entry' 					=> "Eintrag",
+		'entries' 					=> "Einträge",
+		'no_entries' 				=> "Es wurden leider noch keine<br>Einträge hinterlassen.",
+		'entries_on_pages' 			=> "Einträge auf {PAGES} Seiten",
+		'page_first' 				=> "Zur ersten Seite",
+		'page_first_symbol' 		=> "&laquo;",
+		'page_forwards' 			=> "Eine Seite vorwärts blättern",
+		'page_forwards_symbol' 		=> "&rsaquo;",
+		'page_last' 				=> "Zur letzten Seite",
+		'page_last_symbol' 			=> "&raquo;",
+		'page_backwards' 			=> "Eine Seite zurück blättern",
+		'page_backwards_symbol' 	=> "&lsaquo;",
+		'anchor'					=> "Direkt zu diesem Eintrag springen",
+		'from' 						=> "aus",
+		'at' 						=> "um",
+		'oclock' 					=> "Uhr",
+		'comment' 					=> "Kommentar",
+		'email_yes' 				=> "eMail von {ENTRY_NAME}",
+		'email_no' 					=> "{ENTRY_NAME} möchte keine eMails über das Gästebuch empfangen.",
+		'hp_of' 					=> "Homepage von {ENTRY_NAME}",
+		'gravatar' 					=> "Gravatar von {ENTRY_NAME}",
+		'quote' 					=> "Zitat von",
 
 	// NEWENTRY.PHP
-	$lang['new_entry_name'] = "Dein Name:";
-	$lang['new_entry_city'] = "Wohnort:";
-	$lang['new_entry_email'] = "eMail:";
-	$lang['new_entry_mastodon'] = "Mastodon:";
-	$lang['new_entry_bluesky'] = "Bluesky:";
-	$lang['new_entry_w'] = "W:";
-	$lang['new_entry_eu_vision'] = "EU Vision:";
-	$lang['new_entry_eu_video'] = "EU Video:";
-	$lang['new_entry_eu_monnett'] = "EU Monnett:";
-	$lang['new_entry_hp'] = "Homepage:";
-	$lang['new_entry_message'] = "Deine Nachricht:";
-	$lang['necessary_fields'] = "[ Pflichtfelder sind mit einem Stern (*) gekennzeichnet ]";
-	$lang['user_notification'] = "Per eMail benachrichtigen, wenn der Eintrag freigeschaltet, oder ein Kommentar dazu geschrieben wurde.";
-	$lang['user_show_email'] = "Ermögliche anderen Benutzern mir eine eMail über das Kontaktformular zu schreiben. Um Spam vorzubeugen wird meine Emailadresse nicht angezeigt.";
-	$lang['user_accept_akismet_service'] = "Dieser Eintrag wird durch 'Akismet' auf Spam überprüft. Ich bin mir bewusst, dass wenn ich den Eintrag absende, persönliche Daten von mir auf einen Server in die USA geschickt werden, und akzeptiere dies.";
-	$lang['send'] = "Eintragen";
-	$lang['preview'] = "Vorschau";
-	$lang['security_code'] = "Sicherheitscode";
-	$lang['captcha_refresh'] = "Neues Captcha generieren";
-	$lang['captcha_what_is_that'] = "Was ist das?";
-	$lang['captcha_wikipedia'] = "http://de.wikipedia.org/wiki/Captcha";
-	$lang['captcha_tooltip'] = "Ein neuer Eintrag erfordert die Eingabe eines Sicherheitscodes um automatisierte Eintragungen zu vermeiden. Bitte tippe alle Buchstaben GROSS ein. Sollte der Code unleserlich sein, lasse das Textfeld leer, und klicke auf ''Eintragen''. Dann wird ein neuer Code generiert. Deine bisherigen Eingaben bleiben dabei erhalten. Sollte kein neuer Code generiert werden, klicke bitte rechts und dann auf ''Aktualisieren''.";
-	$lang['back_to_mainpage'] = "Zurück zur Hauptseite";
-	$lang['back'] = "Zurück";
-	$lang['entry_success_mod'] = "Dein Eintrag wurde erfolgreich gespeichert.<br>Er wird vom Admin begutachtet, und dann freigeschaltet werden.";
-	$lang['entry_success'] = "Dein Eintrag wurde erfolgreich gespeichert. Du kannst ihn Dir sofort ansehen.";
-	$lang['forwarding'] = "Du wirst in {REFRESH_TIME} Sekunden automatisch weitergeleitet. Wenn nicht klicke bitte auf ''Zurück zur Hauptseite''.";
-	$lang['sendmail_admin_title'] = "Neuer Gästebucheintrag von '{NAME}'";
-	$lang['sendmail_user_title'] = "Dein Eintrag auf {DOMAIN}";
+	'new_entry_name' 				=> "Dein Name:",
+	'new_entry_city' 				=> "Wohnort:",
+	'new_entry_email' 				=> "eMail:",
+	'new_entry_mastodon' 			=> "Mastodon:",
+	'new_entry_bluesky' 			=> "Bluesky:",
+	'new_entry_w' 					=> "W:",
+	'new_entry_eu_vision' 			=> "EU Vision:",
+	'new_entry_eu_video' 			=> "EU Video:",
+	'new_entry_eu_monnett' 			=> "EU Monnett:",
+	'new_entry_hp' 					=> "Homepage:",
+	'new_entry_message' 			=> "Deine Nachricht:",
+	'necessary_fields' 				=> "[ Pflichtfelder sind mit einem Stern (*) gekennzeichnet ]",
+	'user_notification' 			=> "Per eMail benachrichtigen, wenn der Eintrag freigeschaltet, oder ein Kommentar dazu geschrieben wurde.",
+	'user_show_email' 				=> "Ermögliche anderen Benutzern mir eine eMail über das Kontaktformular zu schreiben. Um Spam vorzubeugen wird meine Emailadresse nicht angezeigt.",
+	'user_accept_akismet_service' 	=> "Dieser Eintrag wird durch 'Akismet' auf Spam überprüft. Ich bin mir bewusst, dass wenn ich den Eintrag absende, persönliche Daten von mir auf einen Server in die USA geschickt werden, und akzeptiere dies.",
+	'send' 							=> "Eintragen",
+	'preview' 						=> "Vorschau",
+	'security_code' 				=> "Sicherheitscode",
+	'captcha_refresh' 				=> "Neues Captcha generieren",
+	'captcha_what_is_that' 			=> "Was ist das?",
+	'captcha_wikipedia' 			=> "https://de.wikipedia.org/wiki/Captcha",
+	'captcha_tooltip' 				=> "Ein neuer Eintrag erfordert die Eingabe eines Sicherheitscodes um automatisierte Eintragungen zu vermeiden. Bitte tippe alle Buchstaben GROSS ein. Sollte der Code unleserlich sein, lasse das Textfeld leer, und klicke auf ''Eintragen''. Dann wird ein neuer Code generiert. Deine bisherigen Eingaben bleiben dabei erhalten. Sollte kein neuer Code generiert werden, klicke bitte rechts und dann auf ''Aktualisieren''.",
+	'back_to_mainpage' 				=> "Zurück zur Hauptseite",
+	'back' 							=> "Zurück",
+	'entry_success_mod' 			=> "Dein Eintrag wurde erfolgreich gespeichert.<br>Er wird vom Admin begutachtet, und dann freigeschaltet werden.",
+	'entry_success' 				=> "Dein Eintrag wurde erfolgreich gespeichert. Du kannst ihn Dir sofort ansehen.",
+	'forwarding' 					=> "Du wirst in {REFRESH_TIME} Sekunden automatisch weitergeleitet. Wenn nicht klicke bitte auf ''Zurück zur Hauptseite''.",
+	'sendmail_admin_title' 			=> "Neuer Gästebucheintrag von '{NAME}'",
+	'sendmail_user_title' 			=> "Dein Eintrag auf {DOMAIN}",
 
 	// EMAIL.PHP
-	$lang['email_name'] = "Dein Name:";
-	$lang['email_email'] = "Deine eMail:";
-	$lang['email_message'] = "Deine Nachricht:";
-	$lang['email_sent_to'] = "Diese eMail wird geschickt an:";
-	$lang['email_send'] = "Absenden";
-	$lang['email_caption'] = "eMail von '{NAME}' über das Gästebuch von {DOMAIN}";
-	$lang['email_caption_copy'] = "eMail an '{NAME}' über das Gästebuch von {DOMAIN} - Kopie der Nachricht";
-	$lang['email_sender'] = "Absender:";
-	$lang['email_receiver'] = "Empfänger:";
-	$lang['email_from'] = "über:";
-	$lang['email_sendcopytome'] = "Ich möchte eine Kopie dieser eMail erhalten.";
-	$lang['email_success'] = "Deine eMail wurde erfolgreich an den Benutzer verschickt.";
-	$lang['email_fail'] = "Die eMail konnte nicht verschickt werden. Möglicherweise gibt es ein Problem mit dem Mailserver.";
+	'email_name' 			=> "Dein Name:",
+	'email_email' 			=> "Deine eMail:",
+	'email_message' 		=> "Deine Nachricht:",
+	'email_sent_to' 		=> "Diese eMail wird geschickt an:",
+	'email_send' 			=> "Absenden",
+	'email_caption' 		=> "eMail von '{NAME}' über das Gästebuch von {DOMAIN}",
+	'email_caption_copy' 	=> "eMail an '{NAME}' über das Gästebuch von {DOMAIN} - Kopie der Nachricht",
+	'email_sender' 			=> "Absender:",
+	'email_receiver' 		=> "Empfänger:",
+	'email_from' 			=> "über:",
+	'email_sendcopytome' 	=> "Ich möchte eine Kopie dieser eMail erhalten.",
+	'email_success' 		=> "Deine eMail wurde erfolgreich an den Benutzer verschickt.",
+	'email_fail' 			=> "Die eMail konnte nicht verschickt werden. Möglicherweise gibt es ein Problem mit dem Mailserver.",
 
 	// ERRORMESSAGES
-	$lang['errormessage'][1] = "Bitte gib eine Nachricht ein!";
-	$lang['errormessage'][2] = "Bitte gib eine gültige eMail Adresse ein!";
-	$lang['errormessage'][3] = "Bitte gib einen Namen ein!";
-	$lang['errormessage'][4] = "ist keine gültige<br>eMail Adresse!";
-	$lang['errormessage'][5] = "ist keine gültiger<br> Mastodon-Account!";
-	$lang['errormessage'][6] = "Die IP Sperre verbietet einen weiteren Eintrag!";
-	$lang['errormessage'][7] = "Der Sicherheitscode wurde falsch oder nicht eingegeben!";
-	$lang['errormessage'][8] = "Dieser Benutzer möchte keine eMails empfangen!";
-	$lang['errormessage'][9] = "Es ist ein Fehler beim Versand der eMail aufgetreten!";
-	$lang['errormessage'][10] = "Spamschutz: Das Formular wurde zu schnell abgesendet. Bitte warte noch {TIME_LOCK_REST} Sekunden.";
-	$lang['errormessage'][11] = "Die Akismet-Einverständniserklärung wurde nicht akzeptiert.<br>Um den Eintrag übernehmen zu können, muss sie akzeptiert werden.";
-	$lang['errormessage'][12] = "Diese eMail ist für Eintragungen gesperrt.";
-	$lang['errormessage'][13] = "Dieser Domainbereich ist für Eintragungen gesperrt.";
-	$lang['errormessage'][14] = "Diese IP-Adresse ist für Eintragungen gesperrt.";
-	$lang['errormessage'][15] = "ist kein gültiger Facebook Name. Bitte beachten: Es dürfen keine Sonderzeichen und/oder Umlaute enthalten sein! 'ä' wird z.B. zu 'a'.";
-	$lang['errormessage'][16] = "ist kein gültiger Twitter Name. Bitte beachten: Es dürfen keine Sonderzeichen und/oder Umlaute enthalten sein! 'ä' wird z.B. zu 'a'.";
-	$lang['errormessage'][17] = "Die wiederholte, sehr schnelle Tippgeschwindigkeit weist daraufhin, dass Du ein Spamroboter bist. Du wurdest für {KEYSTROKE_BAN_TIME} Sekunden geblockt. Sollte dies ein Missverständnis sein, kannst Du Dich beim Administrator melden.";
-	$lang['errormessage'][18] = "Du wurdest für zu schnelles Tippen geblockt. Der Verdacht liegt nahe, dass Du ein Spamroboter bist. Bitte warte noch {KEYSTROKE_BAN_TIME_REST} Sekunden.";
-	$lang['errormessage'][19] = "Direkte Aufrufe sind nicht gestattet.";
+	'errormessage' => [
+		1 	=> "Bitte gib eine Nachricht ein!",
+		2 	=> "Bitte gib eine gültige eMail Adresse ein!",
+		3 	=> "Bitte gib einen Namen ein!",
+		4 	=> "ist keine gültige<br>eMail Adresse!",
+		5 	=> "ist keine gültiger<br> Mastodon-Account!",
+		6 	=> "Die IP Sperre verbietet einen weiteren Eintrag!",
+		7 	=> "Der Sicherheitscode wurde falsch oder nicht eingegeben!",
+		8 	=> "Dieser Benutzer möchte keine eMails empfangen!",
+		9 	=> "Es ist ein Fehler beim Versand der eMail aufgetreten!",
+		10 	=> "Spamschutz: Das Formular wurde zu schnell abgesendet. Bitte warte noch {TIME_LOCK_REST} Sekunden.",
+		11 	=> "Die Akismet-Einverständniserklärung wurde nicht akzeptiert.<br>Um den Eintrag übernehmen zu können, muss sie akzeptiert werden.",
+		12 	=> "Diese eMail ist für Eintragungen gesperrt.",
+		13 	=> "Dieser Domainbereich ist für Eintragungen gesperrt.",
+		14 	=> "Diese IP-Adresse ist für Eintragungen gesperrt.",
+		15 	=> "ist kein gültiger Facebook Name. Bitte beachten: Es dürfen keine Sonderzeichen und/oder Umlaute enthalten sein! 'ä' wird z.B. zu 'a'.",
+		16 	=> "ist kein gültiger Twitter Name. Bitte beachten: Es dürfen keine Sonderzeichen und/oder Umlaute enthalten sein! 'ä' wird z.B. zu 'a'.",
+		17 	=> "Die wiederholte, sehr schnelle Tippgeschwindigkeit weist daraufhin, dass Du ein Spamroboter bist. Du wurdest für {KEYSTROKE_BAN_TIME} Sekunden geblockt. Sollte dies ein Missverständnis sein, kannst Du Dich beim Administrator melden.",
+		18 	=> "Du wurdest für zu schnelles Tippen geblockt. Der Verdacht liegt nahe, dass Du ein Spamroboter bist. Bitte warte noch {KEYSTROKE_BAN_TIME_REST} Sekunden.",
+		19 	=> "Direkte Aufrufe sind nicht gestattet."
+	],
 
 	// BBCODES
-	$lang['bbcodes'] = "BBCodes:";
-	$lang['bbcode_bold'] = "Fett";
-	$lang['bbcode_help_bold'] = "Fette Darstellung des Textes";
-	$lang['bbcode_italic'] = "Kursiv";
-	$lang['bbcode_help_italic'] = "Kursive Darstellung des Textes";
-	$lang['bbcode_url'] = "URL";
-	$lang['bbcode_help_url'] = "Fügt einen Hyperlink ein. Möglich sind: [url]http://www.test.de/[/url] oder [url=http://www.test.de/]Test[/url] oder [url=http://www.test.de/][img]Adresse zum Bild[/img][/url]";
-	$lang['bbcode_img'] = "Grafik";
-	$lang['bbcode_help_img'] = "Fügt ein Bild ein. Möglich sind: [img]Adresse zum Bild[/img] oder [img=Breite,Höhe]Adresse zum Bild[/img]";
-	$lang['bbcode_flash'] = "Flash";
-	$lang['bbcode_help_flash'] = "Fügt ein Flashvideo ein. -> [flash=Breite,Höhe]URL[/flash]";
-	$lang['bbcode_quote'] = "Zitat";
-	$lang['bbcode_help_quote'] = "Fügt ein Zitat ein. Möglich sind: [quote]Zitat[/quote] oder [quote=Name des Zitierten]Zitat[/quote]";
-	$lang['bbcode_textsize'] = "Schriftgröße";
-	$lang['bbcode_extrasmall'] = "Winzig";
-	$lang['bbcode_small'] = "Klein";
-	$lang['bbcode_default'] = "Standard";
-	$lang['bbcode_big'] = "Groß";
-	$lang['bbcode_extrabig'] = "Riesig";
-	$lang['bbcode_textcolor'] = "Schriftfarbe";
-	$lang['bbcode_help_size'] = "Schriftgrösse";
-	$lang['smileys'] = "Smilies:";
+	'bbcodes' 				=> "BBCodes:",
+	'bbcode_bold' 			=> "Fett",
+	'bbcode_help_bold' 		=> "Fette Darstellung des Textes",
+	'bbcode_italic' 		=> "Kursiv",
+	'bbcode_help_italic' 	=> "Kursive Darstellung des Textes",
+	'bbcode_url' 			=> "URL",
+	'bbcode_help_url' 		=> "Fügt einen Hyperlink ein. Möglich sind: [url]http://www.test.de/[/url] oder [url=http://www.test.de/]Test[/url] oder [url=http://www.test.de/][img]Adresse zum Bild[/img][/url]",
+	'bbcode_img' 			=> "Grafik",
+	'bbcode_help_img' 		=> "Fügt ein Bild ein. Möglich sind: [img]Adresse zum Bild[/img] oder [img=Breite,Höhe]Adresse zum Bild[/img]",
+	'bbcode_flash' 			=> "Flash",
+	'bbcode_help_flash' 	=> "Fügt ein Flashvideo ein. -> [flash=Breite,Höhe]URL[/flash]",
+	'bbcode_quote' 			=> "Zitat",
+	'bbcode_help_quote' 	=> "Fügt ein Zitat ein. Möglich sind: [quote]Zitat[/quote] oder [quote=Name des Zitierten]Zitat[/quote]",
+	'bbcode_textsize' 		=> "Schriftgröße",
+	'bbcode_extrasmall' 	=> "Winzig",
+	'bbcode_small' 			=> "Klein",
+	'bbcode_default' 		=> "Standard",
+	'bbcode_big' 			=> "Groß",
+	'bbcode_extrabig' 		=> "Riesig",
+	'bbcode_textcolor' 		=> "Schriftfarbe",
+	'bbcode_help_size' 		=> "Schriftgrösse",
+	'smileys' 				=> "Smilies:"
+	];
 ?>
